7b6e71907bfc738cd43e55ca7b1433132aa8e1b0
15757126256 <790095673@qq.com>
2019-02-14 21:45:20 +0800

    增加了add按钮

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    推荐页面
+  <div class="recommond">
+    <div class="add-pic">
+      <image src="../../static/images/index/add.png"></image>
+    </div>
   </div>
 </template>
 
@@ -9,8 +11,7 @@
 export default {
   data () {
     return {
-      motto: 'Hello World',
-      userInfo: {}
+      
     }
   },
   components: {
@@ -26,5 +27,27 @@ export default {
 </script>
 
 <style scoped>
-
+.recommond{
+   background-color: #eeeeee;
+    position: absolute;
+    height: 100%;
+    width: 100%
+}
+.add-pic{
+  position: absolute;
+  height: 100rpx;
+  width: 100rpx;
+  border-radius: 60%;
+  margin: 0 auto;
+  background-color: #13AA05;
+  bottom: 40rpx;
+  left: 50%;
+  margin-left: -50rpx;
+  box-shadow: 0 0 20rpx  rgb(175, 175, 175);
+}
+.add-pic image{
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
 </style>
